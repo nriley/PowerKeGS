22d811d2922be1c708283de26cf5eda06f00ee2b
Nicholas Riley <com-github@sabi.net>
2025-06-27 23:13:23 -0400

    Very minimal, probably non-working version.

---
+++ make.s
@@ -0,0 +1,16 @@
+*
+* PowerKeGS
+* Make
+*
+
+* Expansion linker file
+
+                        DSK PowerKeGS
+                        TYP $B3
+
+* Assemble files
+
+                        ASM powerkey.s
+                        KND $0000
+                        SNA PowerKeGS
+
