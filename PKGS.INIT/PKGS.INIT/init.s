0741661325f4c2ab366ecc2e96682f160ccc213b
Nicholas Riley <com-github@sabi.net>
2026-07-02 20:55:21 -0400

    Standardize spacing.

--- PKGS.INIT/PKGS.INIT/init.s
+++ PKGS.INIT/PKGS.INIT/init.s
@@ -1,22 +1,22 @@
-	case	on
+	case on
 
 dummy	private
-	bra	InitStart
+	bra InitStart
 	end
 
 ; As documented in Apple II File Type Note for File Type $B6
 ; in order to permit unloading if hardware is not detected
 unloadFlagPtr data
-	ds	4
+	ds 4
 	end
 
 InitStart private
 	tay
 	tsc
 	clc
-	adc	#4
-	sta	>unloadFlagPtr
-	lda	#0
-	sta	>unloadFlagPtr+2
+	adc #4
+	sta >unloadFlagPtr
+	lda #0
+	sta >unloadFlagPtr+2
 	tya
 	end
