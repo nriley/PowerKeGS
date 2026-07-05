18d999b362c81ae702039ee635af9c03a157ffac
Nicholas Riley <com-github@sabi.net>
2026-07-05 09:43:45 -0400

    Ignore build directory (when building with xcodebuild).

---
+++ PKGS.INIT/PKGS.INIT/pkgs.c
@@ -0,0 +1,9 @@
+/*
+ *  pkgs.c
+ *  PKGS.Init
+ *
+ * Created by Nicholas Riley on 7/3/2026.
+ * 
+ */
+
+#include "pkgs.h"
---
+++ PKGS.INIT/PKGS.INIT/pkgs.h
@@ -0,0 +1,14 @@
+/*
+ *  pkgs.h
+ *  PKGS.Init
+ *
+ *  Created by Nicholas Riley on 7/3/2026.
+ * 
+ */
+
+#ifndef _GUARD_PROJECTPKGS_Init_FILEpkgs_
+#define _GUARD_PROJECTPKGS_Init_FILEpkgs_
+
+
+
+#endif /* define _GUARD_PROJECTPKGS_Init_FILEpkgs_ */
